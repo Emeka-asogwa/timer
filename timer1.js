58918eb8a7b18330dd43eb164c30c3a7d00445a7
Chukwuemeka (Emeka) Asogwa <menco23real@yahoo.com>
2021-06-01 21:16:14 +0000

    ✍🏽Added timer1 code

---
+++ timer1.js
@@ -0,0 +1,13 @@
+const args = process.argv.slice(2).filter(time => !isNaN(time)).filter(time =>  time >= 0);
+
+console.log(args);
+args.forEach((time) => {
+  setTimeout(() => {
+    process.stdout.write('\x07');
+
+  }, time * 1000);
+});
+
+
+
+
